5cc1b70442d4ecde511ea0d38c4efbf97ba9d6a1
AkoZhu <zxuanbiao@outlook.com>
2024-06-12 13:35:56 +0800

    feat: Add ddp training to accelerate

--- src/train.py
+++ src/train.py
@@ -2,16 +2,52 @@ from model.gpt2 import GPT, GPTConfig
 from utils import DataLoaderLite
 from utils import get_lr
 import torch
+import os 
+# --------------
+from torch.distributed import init_process_group, destroy_process_group
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
 
 # we start to train the gpt2 model
 
 # attempt to use the gpt if available
-device = 'cpu'
-if torch.cuda.is_available():
-    device = 'cuda'
-elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
-    device = 'mps'
-print(f"using device:{device}")
+# device = 'cpu'
+# if torch.cuda.is_available():
+#     device = 'cuda'
+# elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+#     device = 'mps'
+# print(f"using device:{device}")
+
+# simple run
+# python train.py
+# DDP launch for e.g. 8 gpu
+# torchrun --standalone --nproc_per_node=8 train.py
+
+# Set up the DDP(distributed data parallel).
+# torchrun command sets the env variables like RANK, LOCAL_RANK, and WORLD_SIZE
+ddp = int(os.environ.get('RANK', -1)) != -1 # is this a DDP process?
+if ddp:
+    # use DDP atm demands CUDA, we set the dev ice appropriately according to rank
+    assert torch.cuda.is_available(), "DDP requires CUDA"
+    init_process_group(backend='nccl')
+    ddp_rank = int(os.environ['RANK'])
+    ddp_world_size = int(os.environ['WORLD_SIZE'])
+    ddp_local_rank = int(os.environ['LOCAL_RANK'])
+    device = f'cuda:{ddp_local_rank}'
+    torch.cuda.set_device(device)
+    master_process = ddp_rank == 0
+else:
+    # if not DDP, we use the CPU or GPU
+    ddp_rank = 0
+    ddp_world_size = 1
+    ddp_local_rank = 0
+    master_process = True
+    device = 'cpu'
+    if torch.cuda.is_available():
+        device = 'cuda'
+    elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+        device = 'mps'
+    print(f"using device:{device}")
 
 # setting seeds
 torch.manual_seed(1337)
@@ -40,17 +76,22 @@ if torch.cuda.is_available():
 total_batch_size = 524288   # 2**19, ~0.5M tokens, in GPT-3
 B = 16                      # our micro batch size
 T = 1024                    # sequence length
-assert total_batch_size % (B*T) == 0, "make sure total_batch_size is divisible by B*T"
-grad_accu_steps = total_batch_size // (B*T)
-print(f"The total desired_batch_size={total_batch_size}, with grad_accu_steps={grad_accu_steps}")
+assert total_batch_size % (B * T * ddp_world_size) == 0, "make sure total_batch_size is divisible by B * T * ddp_world_size"
+grad_accu_steps = total_batch_size // (B * T * ddp_world_size)
+if master_process:
+    print(f"The total desired_batch_size={total_batch_size}, with grad_accu_steps={grad_accu_steps}")
 
 # get the dataset
-train_loader = DataLoaderLite(B=B, T=T)
+train_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size)
 
-# get logits
+# create the model
 model = GPT(GPTConfig(vocab_size=50304)) # fix the ugly number of vocab_size
 model.to(device)
 model = torch.compile(model) # like gcc and make it faster, kernel fusion
+# wrap model using ddp
+if ddp:
+    model = DDP(model, device_ids=[ddp_local_rank]) # ddp helps you average all backward gradient among gpus.
+raw_model = model.module if ddp else model          # always contains the "raw" unwrapped model
 
 # setting less precise and faster
 torch.set_float32_matmul_precision("high") # by default it is highest
@@ -71,7 +112,7 @@ max_steps = 50
 # use the GPT-3 hyperparameters
 # optimizer = torch.optim.AdamW(model.parameters(), lr=max_lr, betas=(0.9, 0.95), eps=1e-8)
 # using the weight decay
-optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
+optimizer = raw_model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
 
 for i in range(max_steps):
     t0 = time.time()
@@ -87,7 +128,13 @@ for i in range(max_steps):
             logits, loss = model(x, y)
         loss /= grad_accu_steps # make sure the loss is averaged
         loss_accum += loss.detach() # accumulate the loss for printing.
+        # only sync the gradient at the very last micro step 
+        if ddp:
+            model.require_backward_grad_sync = (micro_step == grad_accu_steps - 1)
         loss.backward()
+        # make the loss_accum to aggregate the loss from all processes rather than just th local.
+        if ddp:
+            dist.all_reduce(loss_accum, op=dist.ReduceOp.AVG)
     # clip the global norm of the gradient at 1.0
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
     
@@ -105,10 +152,14 @@ for i in range(max_steps):
         torch.cuda.synchronize() # wait for the GPU to finish work
     t1 = time.time()
     dt = t1 - t0 # time difference in seconds
-    tokens_processed = train_loader.B * train_loader.T * grad_accu_steps
+    tokens_processed = train_loader.B * train_loader.T * grad_accu_steps * ddp_world_size
     tokens_per_sec = tokens_processed / dt
-    print(f"step {i} | lr:{lr} | loss:{loss_accum.item():.6f} | dt:{dt} | tokens/sec:{tokens_per_sec:.0f}")
+    if master_process:
+        print(f"step {i} | lr:{lr} | loss:{loss_accum.item():.6f} | dt:{dt} | tokens/sec:{tokens_per_sec:.0f}")
 
+
+if ddp:
+    destroy_process_group()
 
 import sys; sys.exit(0)
 
